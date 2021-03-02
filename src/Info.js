d5c2c8852e5a4ac405369349a8397e6d1b18abe3
rebeca <kattantoledo@gmail.com>
2021-03-01 20:07:34 -0600

    adicion de 2 elementos a app.js y modificacion de app.css

---
+++ src/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Info.css';
+
+function Info(){
+    return (
+        <div class="container estilo">
+            <h1><em>REBECA KATTÁN</em></h1>
+            <h2>Email: kattantoledo@gmail.com</h2>
+        </div>
+    )
+}
+
+export default Info;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Info from './Info'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Info />
   </React.StrictMode>,
   document.getElementById('root')
 );
